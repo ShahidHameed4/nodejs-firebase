b7276395fd02cca182930f6caaa8f649cc8bf44f
shhdh <90132007+ShahidHameed4@users.noreply.github.com>
2023-04-03 17:28:24 +0500

    own api keys addition

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const firebase = require("firebase");
 const firebaseConfig = {
-  apiKey: "AIzaSyACYJ5Cm2Y-i7HJes2ET2QW67zvIgLrZeo",
-  authDomain: "tutorial-e06c1.firebaseapp.com",
-  projectId: "tutorial-e06c1",
-  storageBucket: "tutorial-e06c1.appspot.com",
-  messagingSenderId: "863505404334",
-  appId: "1:863505404334:web:132118b0d6acd7dfa3f83f",
-  measurementId: "G-SW2M0FYQG5",
+  apiKey: "AIzaSyCczYXwWcm4STjAhmSe32xZNlTpaEmWl94",
+  authDomain: "seventh-ripsaw-370217.firebaseapp.com",
+  projectId: "seventh-ripsaw-370217",
+  storageBucket: "seventh-ripsaw-370217.appspot.com",
+  messagingSenderId: "165167665435",
+  appId: "1:165167665435:web:38ebae4135d8921a0543be",
+  measurementId: "G-BFPWTN2M22"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
